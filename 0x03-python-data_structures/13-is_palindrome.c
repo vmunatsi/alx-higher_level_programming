d95c0a4d47f7476bbadab5d3e9c5f880cc3ab10c
Vincent <vincentm43@gmail.com>
2022-08-29 00:29:05 -0700

    task13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,10 +1,10 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- * reverse - Reverse a linked list.
- * @head_medium: Head of the linked list.
- * Return: void.
- */
+ *  * reverse - Reverse a linked list.
+ *   * @head_medium: Head of the linked list.
+ *    * Return: void.
+ *     */
 void reverse(listint_t **head_medium)
 {
 	listint_t *current = *head_medium, *next = NULL, *prev = NULL;
@@ -15,36 +15,35 @@ void reverse(listint_t **head_medium)
 		current->next = prev;
 		prev = current;
 		current = next;
-																}
+	}
 	*head_medium = prev;
 }
 
 /**
-  is_palindrome - Check if a linked list is a palindrome.
- * @head: Head of the linked list.
- * Return: 1 if it is, 0 if doesn't.
- */
+ *  * is_palindrome - Check if a linked list is a palindrome.
+ *   * @head: Head of the linked list.
+ *    * Return: 1 if it is, 0 if doesn't.
+ *     */
 int is_palindrome(listint_t **head)
 {
 	listint_t *fast, *slow, *medium, *current;
 
-	fast = *head;
-	slow = *head;
-	while (fast != NULL && fast->next != NULL)
-	{
-		fast = fast->next->next;
-		slow = slow->next;
-	}
-	medium = slow;
-	reverse(&medium);
-	current = *head;
-	while (medium != NULL)
-	{
-		if (medium->n != current->n)
-																		return (0);
-
-			medium = medium->next;
-																		current = current->next;
-																			}
-										return (1);
+		fast = *head;
+		slow = *head;
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+		}
+		medium = slow;
+		reverse(&medium);
+		current = *head;
+		while (medium != NULL)
+		{
+			if (medium->n != current->n)
+				return (0);
+																		medium = medium->next;
+			current = current->next;
+																	}
+		return (1);
 }
