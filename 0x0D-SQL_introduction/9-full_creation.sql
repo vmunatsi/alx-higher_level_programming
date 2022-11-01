35a016e8ecce6cf85b16dc545eaa9e317b953803
Vincent <vincentm43@gmail.com>
2022-11-01 09:05:06 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,5 @@
+-- inserts a new row in the table first_table 
+-- database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
