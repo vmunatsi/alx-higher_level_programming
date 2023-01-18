0cd14905bebc644f15067c51e27aaecabe56e43c
vmunatsi <vincentm43@gmail.com>
2023-01-17 17:22:30 -0800

    task 1 fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,9 @@
-
 #!/usr/bin/node
-// Write in a file
 
 const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
