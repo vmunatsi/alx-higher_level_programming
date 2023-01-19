8bee5577dbe77ae16385f04c914e17598441c283
vmunatsi <vincentm43@gmail.com>
2023-01-19 04:50:01 -0800

    tasks submitted

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays only body of a 200 status code response
+# This script takes in a URL, sends GET request and displays body of response
 curl -sL "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes delete request
-curl -sD - -o /dev/null "$1" | grep "Allow" | cut -d ":" -f2 | cut -b 2-
+# Script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
